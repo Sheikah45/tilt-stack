70826d9617bcc3d5dd45e638620b3d324f80f8e5
Sheikah45 <Sheikah450@gmail.com>
2024-07-01 00:02:51 -0400

    add user service

--- Tiltfile
+++ Tiltfile
@@ -1,13 +1,17 @@
-load('ext://deployment', 'job_create')
+load('ext://git_resource', 'git_checkout', 'deploy_from_dir')
 
 def get_pod(pod_name):
     return str(local("tilt get kd " + pod_name + " -ojsonpath='{.status.pods[0].name}'"))[1:-1]
 
 k8s_yaml("deploy/mariadb.yaml")
 k8s_yaml("deploy/ory-hydra.yaml")
+k8s_yaml("deploy/faf-user-service.yaml")
 
-local_resource(name = "setup-db", cmd = ["./scripts/setup-db.sh", get_pod("faf-db")], cmd_bat = [".\\scripts\\setup-db.bat", get_pod("faf-db")])
+local_resource(name = "setup-db", cmd = [os.path.abspath("scripts/setup-db.sh"), get_pod("faf-db")], cmd_bat = [os.path.abspath("scripts/setup-db.bat"), get_pod("faf-db")])
+local_resource(name = "setup-hydra-clients", cmd = [os.path.abspath("scripts/setup-hydra-clients.sh"), get_pod("ory-hydra")], cmd_bat = [os.path.abspath("scripts/setup-hydra-clients.bat"), get_pod("ory-hydra")], resource_deps=["ory-hydra"])
 
 k8s_resource(workload='faf-db', port_forwards="3306")
 k8s_resource(workload="faf-db-migrate", resource_deps=["setup-db"])
-k8s_resource(workload='ory-hydra', port_forwards=["4444", "4445"], resource_deps=["ory-hydra-migrate"])
+k8s_resource(workload='ory-hydra-migrate', resource_deps=["faf-db"])
+k8s_resource(workload='ory-hydra', objects=["ory-hydra:configmap"], port_forwards=["4444", "4445"], resource_deps=["ory-hydra-migrate"])
+k8s_resource(workload='faf-user-service', objects=["faf-user-service:configmap", "faf-user-service-mail-templates"], port_forwards=["8080"], resource_deps=["ory-hydra", "faf-db"])
---
+++ scripts/setup-hydra-clients.bat
@@ -0,0 +1,65 @@
+kubectl exec %1 -- hydra clients delete --endpoint http://127.0.0.1:4445 faf-lobby-server faf-website faf-java-client faf-moderator-client faf-forum
+
+kubectl exec %1 -- hydra clients create ^
+    --skip-tls-verify ^
+    --endpoint http://127.0.0.1:4445 ^
+    --fake-tls-termination ^
+    --id faf-lobby-server ^
+    --name faf-lobby-server ^
+    --secret banana ^
+    --scope write_achievements,write_events ^
+    --token-endpoint-auth-method client_secret_post ^
+    -g client_credentials
+
+kubectl exec %1 -- hydra clients create ^
+    --skip-tls-verify ^
+    --endpoint http://127.0.0.1:4445 ^
+    --fake-tls-termination ^
+    --id faf-website ^
+    --secret banana ^
+    --name faforever.xyz ^
+    --logo-uri https://faforever.com/images/faf-logo.png ^
+    --grant-types authorization_code,refresh_token ^
+    --response-types code ^
+    --scope openid,offline,public_profile,write_account_data,create_user ^
+    --callbacks http://localhost:3000/callback
+
+kubectl exec %1 -- hydra clients create ^
+    --skip-tls-verify ^
+    --endpoint http://127.0.0.1:4445 ^
+    --fake-tls-termination ^
+    --id faf-java-client ^
+    --name fafClient ^
+    --logo-uri https://faforever.com/images/faf-logo.png ^
+    --grant-types authorization_code,refresh_token ^
+    --response-types code ^
+    --scope openid,offline,public_profile,lobby,upload_map,upload_mod ^
+    --callbacks http://127.0.0.1 ^
+    --token-endpoint-auth-method none
+	
+kubectl exec %1 -- hydra clients create ^
+    --skip-tls-verify ^
+    --endpoint http://127.0.0.1:4445 ^
+    --fake-tls-termination ^
+    --id faf-moderator-client ^
+    --name faf-moderator-client ^
+    --logo-uri https://faforever.com/images/faf-logo.png ^
+    --grant-types authorization_code,refresh_token ^
+    --response-types code ^
+    --scope upload_avatar,administrative_actions,read_sensible_userdata,manage_vault ^
+    --callbacks http://127.0.0.1 ^
+    --token-endpoint-auth-method none
+
+kubectl exec %1 -- hydra clients create ^
+    --skip-tls-verify ^
+    --endpoint http://127.0.0.1:4445 ^
+    --fake-tls-termination ^
+    --id faf-forum ^
+    --secret banana ^
+    --name forum.faforever.com ^
+    --logo-uri https://faforever.com/images/faf-logo.png ^
+    --grant-types authorization_code,refresh_token ^
+    --response-types code ^
+    --token-endpoint-auth-method client_secret_post ^
+    --scope openid,public_profile ^
+    --callbacks http://127.0.0.1/auth/faf-nodebb/callback
